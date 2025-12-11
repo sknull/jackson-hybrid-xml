b46656df32cc722f06a646427177a0e57f1f7c5c
Stephan Knull <s.knull@t-online.de>
2025-12-11 16:38:28 +0100

    yet another format for date times...

--- src/main/kotlin/de/visualdigits/hybridxml/module/deserializer/OffsetDateTimeDeserializer.kt
+++ src/main/kotlin/de/visualdigits/hybridxml/module/deserializer/OffsetDateTimeDeserializer.kt
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.JsonDeserializer
 import java.time.LocalDate
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoField
 import java.time.temporal.TemporalQueries
@@ -20,7 +21,9 @@ class OffsetDateTimeDeserializer() : JsonDeserializer<OffsetDateTime>() {
     ): OffsetDateTime {
         val text = ctxt.readValue(p, String::class.java)
         return parseDateTimeWithWeekday(text)
-            ?: OffsetDateTime.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX"))
+            ?: parseOffsetDateTime(text)
+            ?: parseZonedDateTime(text)
+            ?: error("Could not parse fiven string '$text'")
     }
 
     private fun parseDateTimeWithWeekday(text: String): OffsetDateTime? {
@@ -32,6 +35,22 @@ class OffsetDateTimeDeserializer() : JsonDeserializer<OffsetDateTime>() {
             val month = temporal[ChronoField.MONTH_OF_YEAR]
             val day = temporal[ChronoField.DAY_OF_MONTH]
             OffsetDateTime.of(LocalDate.of(year, month, day), localTime, offset)
+        } catch (_: Exception) {
+            null // by means
+        }
+    }
+
+    private fun parseOffsetDateTime(text: String): OffsetDateTime? {
+        return try {
+            OffsetDateTime.parse(text, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX"))
+        } catch (_: Exception) {
+            null // by means
+        }
+    }
+
+    private fun parseZonedDateTime(text: String): OffsetDateTime? {
+        return try {
+            ZonedDateTime.parse(text, DateTimeFormatter.RFC_1123_DATE_TIME).toOffsetDateTime()
         } catch (_: Exception) {
             null // by means
         }
